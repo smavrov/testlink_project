7ebd13a800e6d861c806009743c2311957384a09
smavrov <titos@bigmir.net>
2015-04-15 07:26:23 +0300

    Some changes in TestPlanTest

--- src/test/java/tests/TestplanTest.java
+++ src/test/java/tests/TestplanTest.java
@@ -2,8 +2,10 @@ package tests;
 
 import models.TestPlan;
 import models.User;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -12,6 +14,8 @@ import pages.LoginPage;
 import pages.TestlinkHomePage;
 import pages.TestplanEditPage;
 import pages.TestplanManagementPage;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by admin on 08.04.2015.
@@ -23,6 +27,9 @@ public class TestplanTest {
     @BeforeTest
     public void userLogin() {
         driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(new User());
     }
@@ -55,7 +62,7 @@ public class TestplanTest {
     }
 
     public void logout() {
-        TestlinkHomePage homePage = new TestlinkHomePage();
+        TestlinkHomePage homePage = new TestlinkHomePage(driver);
         homePage.logout();
     }
 }
